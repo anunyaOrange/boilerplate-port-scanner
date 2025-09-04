94c1d72f4013d5711362871c0676a2a362768cf3
anunyaOrange <anunyaOrange@proton.me>
2025-09-04 11:20:58 +0700

    print check

--- port_scanner.py
+++ port_scanner.py
@@ -3,6 +3,9 @@ import socket
 def get_open_ports(target, port_range, verbose = False):
     open_ports = []
 
+    print(target)
+    print(port_range)
+    print(verbose)    
 
 
     return(open_ports)
