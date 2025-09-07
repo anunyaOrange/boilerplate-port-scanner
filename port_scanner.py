1663e641ef768f816656601136db020e00ba8427
anunyaOrange <anunyaOrange@proton.me>
2025-09-07 10:21:26 +0700

    add validator

--- port_scanner.py
+++ port_scanner.py
@@ -1,13 +1,11 @@
 import socket
+import validators
 
 def get_open_ports(target, port_range, verbose = False):
     open_ports = []
-    print(target)
-    print(port_range)
-    print(verbose)
-    if (target):
-        return("Error: Invalid hostname")
-    if (port_range):
+    urlvalid = validators.url(target)
+    if urlvalid:
+        if (port_range):
         return("Error: Invalid IP address")
     if (verbose):
         return """Open ports for scanme.nmap.org (45.33.32.156)
@@ -15,7 +13,5 @@ PORT     SERVICE
 22       ssh
 80       http"""
 
-
-
-
-    return(open_ports)
+    else:
+        return("Error: Invalid hostname")
