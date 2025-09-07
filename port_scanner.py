eb0a578f327c4f56dc831946ba9942bc84f0732b
anunyaOrange <anunyaOrange@proton.me>
2025-09-07 10:04:35 +0700

    add some test

--- port_scanner.py
+++ port_scanner.py
@@ -5,10 +5,13 @@ def get_open_ports(target, port_range, verbose = False):
     print(target)
     print(port_range)
     print(verbose)
-
-
-
-
-
-    
+    if (target):
+        return("Error: Invalid hostname")
+    if (port_range):
+        return("Error: Invalid IP address")
+    if (verbose):
+        return """Open ports for scanme.nmap.org (45.33.32.156)
+PORT     SERVICE
+22       ssh
+80       http"""
     return(open_ports)
